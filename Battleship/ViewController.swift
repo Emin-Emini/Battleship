e3c984e4abfe57f6ba107d4b874f63f15af9a667
Emin Emini <ee37811@ubt-uni.net>
2019-10-15 17:23:35 +0200

    Initial Commit

---
+++ Battleship/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Battleship
+//
+//  Created by Emin Emini on 15.10.19.
+//  Copyright © 2019 Emin Emini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BattleshipTests/BattleshipTests.swift
@@ -0,0 +1,34 @@
+//
+//  BattleshipTests.swift
+//  BattleshipTests
+//
+//  Created by Emin Emini on 15.10.19.
+//  Copyright © 2019 Emin Emini. All rights reserved.
+//
+
+import XCTest
+@testable import Battleship
+
+class BattleshipTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
