d5a4207c65064bd6427e75c911974b8e271f09a1
EminRma <eminrmae@gmail.com>
2020-04-29 03:16:48 +0100

    Test

--- Battleship/Extensions.swift
+++ Battleship/Extensions.swift
@@ -13,6 +13,10 @@ extension UIView{
     
     //MARK: - Animations
     
+    func testing() {
+        print("test")
+    }
+    
     func blink() {
         self.alpha = 0.2
         UIView.animate(withDuration: 1, delay: 0.0, options: [.curveLinear, .repeat, .autoreverse], animations: {self.alpha = 1.0}, completion: nil)
